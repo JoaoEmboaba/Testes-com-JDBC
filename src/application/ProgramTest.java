6cccd927fc01de2b71e09297827e0d0bf093cb1e
JoaoVitor83 <vititor666demon@gmail.com>
2023-04-28 04:53:20 -0300

    Inserção dos comandos de recuperação de dados

--- src/application/ProgramTest.java
+++ src/application/ProgramTest.java
@@ -1,19 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class ProgramTest {
 
 	public static void main(String[] args) {
 
-		Connection cnn = DB.getConnection();
-		if (cnn != null) {
-			System.out.println("Deu certo!");
-		}
+		Connection cnn = null;
+		Statement st = null;
+		ResultSet rs = null;
 
-		DB.closeConnection();
+		
+		/* Comandos para recuperação de dados
+		try {
+			cnn = DB.getConnection();
+			st = cnn.createStatement();
+			rs = st.executeQuery("SELECT * FROM seller");
 
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name") + ", " + rs.getString("Email") + ", "
+						+ rs.getString("BirthDate") + ", " + rs.getString("BirthDate") + ", "
+						+ rs.getString("BaseSalary") + ", " + rs.getString("DepartmentId"));
+			}
+		} catch (SQLException ex) {
+			throw new DbException(ex.getMessage());
+		} finally {
+			DB.closeConnection();
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		} */
 	}
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -34,6 +36,24 @@ public class DB {
 				throw new DbException(ex.getMessage());
 			}
 		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null)
+			try {
+				st.close();
+			} catch (SQLException ex) {
+				throw new DbException(ex.getMessage());
+			}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null)
+			try {
+				rs.close();
+			} catch (SQLException ex) {
+				throw new DbException(ex.getMessage());
+			}
 	}
 
 	private static Properties loadProperties() {
